3f34f537a20bfc4aec3bdf80d7901f8fd79fd2f7
Sofia Esparza-Chavez <sesparzachavez2019@aolp.org>
2021-03-09 22:13:35 -0800

    implemented randomized and quadratic sort

--- quickerQS/src/hybridsort.java
+++ quickerQS/src/hybridsort.java
@@ -1,6 +1,11 @@
 public class hybridsort {
-    public void quadraticsort (double [] arr, int left, int right){
+    public void hybridsort (double [] arr, int left, int right){
 
+    }
+
+    public static void main(String args[]){
+        double[] arr;
+        arr = new double[] {10,16,8,12,15,6,3,9,5};
     }
 }
 
